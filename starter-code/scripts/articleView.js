a6abf4db4990559a8717bd882b6402bf25ecc5c0
Lizabeth Petersen <lizpetersen928@gmail.com>
2017-12-01 17:21:08 -0800

    adds and instantiates the article on the new article page; adds preview functionality on new.html

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -83,70 +83,54 @@ articleView.initIndexPage = () => {
     articleView.setTeasers();
 };
 
-
-
 // COMMENT: Where is this function called? Why?
 // The below function is called in the new.html and, when it functions correctly, will initialize a new page for each new article.
 
 articleView.initNewArticlePage = () => {
     // TODO: Ensure the main .tab-content area is revealed. We might add more tabs later or otherwise edit the tab navigation.
-
-    // The new articles we create will be given to the user as JSON so they can copy/paste it into their source data file.
+    $('.tab-content').show();
+    $('#articles').empty();
     // STRETCH: Hide the export section for now, and show it once we have data to export.
-
+    $('#export-field').hide();
+    // The new articles we create will be given to the user as JSON so they can copy/paste it into their source data file.
     $('#article-json').on('focus', function () {
         this.select();
     });
 
     // TODO: Add an event handler to update the preview (STRETCH: and the export field) if any inputs change.
-    $('#articles').on('keyPress', function() {
-
+    $('#new-article :input').on('change', function() {
+        articleView.create();
     });
 };
-
 
 articleView.create = () => {
-    // TODO: DONE: Set up a variable to hold the new article we are creating.
-    const newArticle = {};
-
     // DONE: Clear out the #articles element, so we can put in the updated preview
-    $('#new-article').empty();
-
-    // TODO: Instantiate an article based on what's in the form fields:
-
-    // select element that has form data (jQuery to select all elements in form and iterate over those)
-    const inputs = $('#new-article :input');
-    //console.log(inputs);
-
-    // get the info from the form 
-    inputs.each(function() {
-    //    console.log($(this).val() );
-
-    // create a new Article (object that does not correspond to anything on the page)
-        rawData.forEach(articleObject => inputs.push(new Article(articleObject)));
-
-        // render article on the page by calling .toHtml
-        inputs.forEach(articleObject => $('#articles').append(newArticle.toHtml()));
-
-        // fill in newArticle object with properties
-        $('#new-article :input').html($(this).val());
+    let article;
+    $('#articles').empty();
+    // DONE: Set up a variable to hold the new article we are creating.
+    article = new Article ({
+        title: $('#new-title').val(),
+        category: $('#new-category').val(),
+        author: $('#new-author').val(),
+        authorUrl: $('#new-website').val(),
+        body: $('#new-body').val(),
+        publishedOn: $('#new-is-published:checked').length ? new Date() : null
     });
-    // STRETCH: Pass the article body into the marked.js library to format our Markdown input
 
-    // TODO: Use our interface to the Handlebars template to put this new article into the DOM:
-    Article.prototype.toHtml = function () {
-        const articleView = Handlebars.compile($('#article-template').html());
+    // DONE: Instantiate an article based on what's in the form fields:
+    $('#articles').append(article.toHtml());
 
-        this.daysAgo = parseInt((new Date() - new Date(this.publishedOn)) / 60 / 60 / 24 / 1000);
-        this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
-
-        return articleView(this);
-    };
-    // STRETCH: Pass the article body into the marked.js library to format our Markdown input
-
+    $('pre code').each(function(i, block) {
+        hljs.highlightBlock(block);
+    });
 };
 
-// STRETCH: Activate the highlighting of any code blocks; look at the documentation for hljs to see how to do this by placing a callback function in the .each():
-// $('pre code').each();
-
-// STRETCH: Show our export field, and export the new article as JSON, so it's ready to copy/paste into blogArticles.js:
+// DONE: Use our interface to the Handlebars template to put this new article into the DOM:
+//Article.prototype.toHtml = function () {
+//    const articleView = Handlebars.compile($('#new-article-template').html());
+//
+//    this.daysAgo = parseInt((new Date() - new Date(this.publishedOn)) / 60 / 60 / 24 / 1000);
+//    this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
+//
+//    return articleView(this);
+//};
